ccba8b1b4d2aa3f1c0dfc8b50cac964e713fb3fd
joseph herrera <micheladagato@hotmail.com>
2020-01-23 12:57:52 -0600

    Added finish project to source control.

---
+++ FibonacciSequence/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FibonacciSequence
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int iFirst = 0;
+			int iSecond = 1;
+			int[] MainArray = new int[10];
+
+			MainArray[0] = iFirst;
+			MainArray[1] = iSecond;
+
+			for(int CurrentNumber = 2; CurrentNumber <= 9; CurrentNumber++)
+			{
+				MainArray[CurrentNumber] = iFirst + iSecond;
+				iFirst = iSecond;
+				iSecond = MainArray[CurrentNumber];
+			}
+
+			foreach(int number in MainArray)
+			{
+				Console.Write($"{number}");
+				Console.Write(",");
+			}
+			Console.WriteLine();
+			Console.ReadLine();
+		}
+	}
+}
